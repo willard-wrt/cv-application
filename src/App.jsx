bbfa3b54a61d4ae0e45a1bf61bf6f679878c654b
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-11-28 00:18:08 +0700

    Add a function with useState for Personal details

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function InputSet({'data-key': dataKey, id, value, type, labelText, onChange, optional, placeholder, recommended}) {
+return (
+  <div className='input-group'>
+    <label htmlFor={id}>
+      <span className='label-text'>{labelText}</span>
+      {optional && <span className='optional-text'>optional</span>}
+      {recommended && <span className='recommended-text'></span>}
+    </label>
+    {type ==='textarea' ? (
+      <textarea id={id} value={value} onChange={onChange} placeholder={placeholder} data-key={dataKey}></textarea>
+    ) : (<input type={type} value={value} id={id} onChange={onChange} placeholder={placeholder} data-key={dataKey}>
+    </input>)
+    }
+  </div>
+)
+}
 
+function PersonalDetails ({fullName, phoneNum, email, address, onChange}) {
+  return (
+    <form className='personal-details'>
+      <h3>Personal Details</h3>
+      <InputSet type='text' id='full-name' labelText='Full Name' placeholder='First and last name' value={fullName} onChange={onChange} data-key='fullName'></InputSet>
+      <InputSet type='tel' id='phone-num' labelText='Phone Number' placeholder='Enter phone number' value={phoneNum} onChange={onChange} data-key='phoneNum' recommended></InputSet>
+      <InputSet type='email' id='email' labelText='Email' placeholder='Enter email' value={email} onChange={onChange} data-key='email' recommended></InputSet>
+      <InputSet type='text' id='address' labelText='Address' placeholder='Enter address' value={address} onChange={onChange} data-key='address' recommended></InputSet>
+    </form>
+  )
+}
+
+function App() {
+  const [personalInfo, setPersonalInfo] = useState('');
+  function handleInfoChange(e) {
+    const key = e.target.dataset;
+    console.log(key);
+    setPersonalInfo({...personalInfo, [key]:e.target.value})
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<PersonalDetails fullName={personalInfo.fullName} phoneNum={personalInfo.phoneNum} email={personalInfo.email} address={personalInfo.address} onChange={handleInfoChange}></PersonalDetails>
     </>
   )
 }
